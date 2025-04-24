8a0c6693c2b6d142c331343d4f8925b352101594
Emre Can Vural <emre.vural@trendyol.com>
2025-04-24 23:51:23 +0300

    Update dojoscript.py

--- dojoscript.py
+++ dojoscript.py
@@ -8,8 +8,7 @@ def main():
     valid_files = {
         "results.sarif": "SARIF",
         "semgrep.sarif": "SARIF",
-        "zap-report.xml" "ZAP Scan"
-
+        "zap-report.xml" "ZAP Scan",
     }
 
     # Eğer komut satırı argümanları verilmişse, onları kullan; aksi halde çalışma dizininde bulunan dosyalar üzerinden devam et.
